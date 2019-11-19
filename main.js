242fc2579fb6f9bfb9defb57cb8a78bb631d524c
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-19 14:07:17 +0100

    Creazione Files

---
+++ main.js
@@ -0,0 +1,5 @@
+// Stampare tutti i numeri da 1 a 100 con le seguenti regole:
+// al posto dei multipli di 3 stampare "Fizz"
+// al posto dei multipli di 5 stampare "Buzz"
+// al posto dei multipli di 3 e 5 stampare "FizzBuzz"
+// nome repo: js-fizzbuzz
