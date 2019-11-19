fb41744e77535fa3932c2cc817042e8b77539547
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-19 14:20:48 +0100

    Esercizio Terminato Concettualmente

--- main.js
+++ main.js
@@ -3,3 +3,20 @@
 // al posto dei multipli di 5 stampare "Buzz"
 // al posto dei multipli di 3 e 5 stampare "FizzBuzz"
 // nome repo: js-fizzbuzz
+
+// creazione array d'appoggio
+var list = []
+// ciclo per creare numeri da 1 a 100
+for (var i = 1; i < 101; i++) {
+    var num = i;
+    list.push(num);
+    if (num % 3 == 0 && num % 5 == 0) {
+        console.log('FizzBuzz');
+    } else if (num % 3 == 0) {
+        console.log('Fizz');
+    } else if (num % 5 == 0) {
+        console.log('Buzz');
+    } else {
+        console.log(num);
+    }
+}
