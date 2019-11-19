6a4c03770dfa8b42e698d3d3c574ccf85e48964a
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-19 15:20:06 +0100

    Lista Creata - Bisogna dargli una stilata

--- main.js
+++ main.js
@@ -5,19 +5,20 @@
 // nome repo: js-fizzbuzz
 
 // creazione array d'appoggio
-var list = []
+var list = ''
 // ciclo per creare numeri da 1 a 100
 for (var i = 1; i < 101; i++) {
     var num = i;
     if (num % 3 == 0 && num % 5 == 0) {
-        num = '<il>' + 'FizzBuzz' + '</il>';
+        list = list + '<li>' + 'FizzBuzz' + '</li>';
     } else if (num % 3 == 0) {
-        num = '<il>' + 'Fizz' + '</il>';
+        list = list + '<li>' + 'Fizz' + '</li>';
     } else if (num % 5 == 0) {
-        num = '<il>' + 'Buzz' + '</il>';
+        list = list + '<li>' + 'Buzz' + '</li>';
     } else {
-        num = '<il>' + num + '</il>';
+        list = list + '<li>' + num + '</li>';
     }
-    list.push(num);
-}
-console.log(list);
+};
+list_unordered = '<ul>' + list + '</ul>';
+console.log(list_unordered);
+document.getElementById('fizzbuzztest').innerHTML = list_unordered;
